384f4882136cfdc44caa270f8bfc2f828958019a
m4th3usfp <141687871+m4th3usfp@users.noreply.github.com>
2024-02-28 21:59:11 -0300

    arrumou identação

--- API_FAKE/api_fake.js
+++ API_FAKE/api_fake.js
@@ -9,15 +9,16 @@ const pessoasSalvas = [{}];
 
 function inserirDados(form) {
 
-    
+
     const nome = form.querySelector('input[name="nome"]').value;
     const email = form.querySelector('input[name="email"]').value;
     const tipo = form.querySelector('input[name="tipo"]:checked').value;
     const URL = document.getElementById('URL').value;
 
-    
+
     if (event.submitter.id === 'cancelar_modal') {
-        return; }    
+        return;
+    }
 
     let value;
     if (tipo === '1') {
@@ -29,63 +30,63 @@ function inserirDados(form) {
     } else {
         value = 'Tipo desconhecido';
     }
-    
-    const novaPessoa = {nome, email, tipo, URL};
+
+    const novaPessoa = { nome, email, tipo, URL };
 
     const url = 'https://crudcrud.com/api/e713465d595040829f9008424d45f426/pessoa';
     fetch(url, {
         method: 'POST',
         headers: {
-          
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          
-  
+
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+
+
         },
         body: JSON.stringify(novaPessoa)
-      })
-      .then((response) => {
-        console.log(response)
-        return response.json();
-      })
-      .then((data) => {
-        pessoasSalvas.push(novaPessoa);
+    })
+        .then((response) => {
+            console.log(response)
+            return response.json();
+        })
+        .then((data) => {
+            pessoasSalvas.push(novaPessoa);
 
 
-const tbody = document.querySelector('#tbody');
+            const tbody = document.querySelector('#tbody');
 
-// Criação da linha de cabeçalho
-if (tbody.querySelectorAll('tr').length === 0) {
-    const headerRow = document.createElement('tr');
-    headerRow.innerHTML = `
+            // Criação da linha de cabeçalho
+            if (tbody.querySelectorAll('tr').length === 0) {
+                const headerRow = document.createElement('tr');
+                headerRow.innerHTML = `
         <td>Nome</td>
         <td>Email</td>
         <td>Tipo</td>
         <td></td>
         </tr>
         `;
-    tbody.appendChild(headerRow);
-    
-}
+                tbody.appendChild(headerRow);
 
-// Criação da nova linha com os dados da pessoa
-const url = 'https://crudcrud.com/api/bedb493ae725448b88476f29ecdef687';
-        fetch(url, {
-          method: 'GET',
-          
-          })
-          
-          .then((response) => {
-              
-            return response.json();
-          })
-          
-        
-        .then((data) => {
-          for (pessoa of data) {
-          const tbody = document.querySelector('#tbody');
-          const newRow = document.createElement('tr');
-          newRow.innerHTML = `
+            }
+
+            // Criação da nova linha com os dados da pessoa
+            const url = 'https://crudcrud.com/api/bedb493ae725448b88476f29ecdef687';
+            fetch(url, {
+                method: 'GET',
+
+            })
+
+                .then((response) => {
+
+                    return response.json();
+                })
+
+
+                .then((data) => {
+                    for (pessoa of data) {
+                        const tbody = document.querySelector('#tbody');
+                        const newRow = document.createElement('tr');
+                        newRow.innerHTML = `
               <td>${nome}</td>
               <td>${email}</td>
               <td>${value}</td>
@@ -95,40 +96,40 @@ const url = 'https://crudcrud.com/api/bedb493ae725448b88476f29ecdef687';
               </td>
               
               `;
-              
-          tbody.appendChild(newRow);
-      
-        }
-        })
-        .catch((error) => {
-            console.log(error)
+
+                        tbody.appendChild(newRow);
+
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+
+            if (tbody.querySelectorAll('tr').length > 1) {
+                document.getElementById('h5').style.display = 'none';
+                alert('Pessoa adicionada');
+            }
+
+
+
+
+
+
+
         });
 
-if (tbody.querySelectorAll('tr').length > 1) {
-    document.getElementById('h5').style.display = 'none';
-    alert('Pessoa adicionada');
+
+
+
 }
-    
 
 
 
-            
-
-
-          });
-
-    
-    
-
-}    
-    
-
-    
 
 function cancelarLinha(botao) {
     // Obtém o elemento <tr> pai do botão de cancelar
     const linha = botao.closest('tr');
-    
+
     // Obtém o elemento <tbody> pai da linha
     const tbody = linha.parentNode;
 
@@ -140,7 +141,7 @@ function cancelarLinha(botao) {
         alert('Tem certeza que deseja remover a pessoa ?');
 
         newRow.remove()
-    } else if (tbody.querySelectorAll('tr').length === 1 ) {
+    } else if (tbody.querySelectorAll('tr').length === 1) {
 
         alert('Tem certeza que deseja remover a pessoa ?');
 
