ad28981dcd8317301369339397a21d5d33fd7ea4
m4th3usfp <141687871+m4th3usfp@users.noreply.github.com>
2024-03-04 19:46:09 -0300

    começando a fazer o delete

--- API_FAKE/api_fake.js
+++ API_FAKE/api_fake.js
@@ -1,5 +1,7 @@
 const pessoasSalvas = [{}];
 
+const idsalvo = [{}];
+
 document.addEventListener('DOMContentLoaded', function () {
     let url = document.querySelector('#URL')
 
@@ -25,7 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     })
                     .then(response => {
-                        console.log('olha aki =>', response)
+                       
+                        
+
+                        console.log('response do GET =>', response)
 
                         for (pessoa of response) {
 
@@ -40,6 +45,9 @@ document.addEventListener('DOMContentLoaded', function () {
                             }
 
                             const tbody = document.querySelector('#tbody');
+                            let pessoa_ID = pessoa._id;
+                            idsalvo.push(pessoa_ID)
+                            console.log(pessoa_ID)
 
                             if (tbody.querySelectorAll('tr').length === 0) {
                                 const headerRow = document.createElement('tr');
@@ -67,13 +75,15 @@ document.addEventListener('DOMContentLoaded', function () {
                                 <td>${pessoa.tipo}</td>
                                 <td>
                                     <button id="editar_pessoa" class="btn btn-secondary" type="button">Editar</button>
-                                    <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" onclick="cancelarLinha(this)">Cancelar</button>
+                                    <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" value=${pessoa_ID} onclick="cancelarLinha(this)">Cancelar</button>
                                 </td>
                                 
                                 `;
 
                             tbody.appendChild(newRow);
-
+                            
+                            
+                            
 
 
                         }
@@ -112,6 +122,8 @@ function inserirDados(form) {
     const email = form.querySelector('input[name="email"]').value;
     let tipo = form.querySelector('input[name="tipo"]:checked').value;
     const URL = document.getElementById('URL').value;
+    
+    
 
 
     if (event.submitter.id === 'cancelar_modal') {
@@ -137,11 +149,14 @@ function inserirDados(form) {
         body: JSON.stringify(novaPessoa)
     })
         .then((response) => {
-            console.log(response)
+            console.log('response post =>',response)
             return response.json();
         })
-        .then(() => {
+        .then((response) => {
+            let pessoa_ID = response._id;
+            console.log(pessoa_ID)
             pessoasSalvas.push(novaPessoa);
+            idsalvo.push(pessoa_ID);
 
 
             const tbody = document.querySelectorAll('#tbody')[0];
@@ -180,13 +195,15 @@ function inserirDados(form) {
               <td>${email}</td>
               <td>${tipo}</td>
               <td>
-                  <button id="editar_pessoa" class="btn btn-secondary" type="button">Editar</button>
-                  <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" onclick="cancelarLinha(this)">Cancelar</button>
+                  <button id="editar_pessoa" class="btn btn-secondary" type="button" onclick= "editarLinha(this)">Editar</button>
+                  <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" value=${pessoa_ID} onclick="cancelarLinha(this)">Cancelar</button>
               </td>
               
               `;
 
             tbody.appendChild(newRow);
+            
+            
 
             if (tbody.querySelectorAll('tr').length >= 1) {
                 document.getElementById('h5').style.display = 'none';
@@ -200,6 +217,9 @@ function inserirDados(form) {
 
         });
 }
+
+
+
 
 
 function cancelarLinha(botao) {
@@ -217,6 +237,7 @@ function cancelarLinha(botao) {
         alert('Tem certeza que deseja remover a pessoa ?');
 
         linha.remove()
+       
     } else if (tbody.querySelectorAll('tr').length === 1) {
 
         alert('Tem certeza que deseja remover a pessoa ?');
@@ -224,11 +245,33 @@ function cancelarLinha(botao) {
         linha.remove();
         tbody.style.display = 'none';
         document.querySelector('h5').style.display = 'block'
-    } if (tbody.querySelector('tr').length > 1) {
-        tbody.style.display = '';
     }
-}
+    
+    
+    const url = document.querySelector('#URL').value;
 
+    let pessoa_ID = document.querySelector('#cancelar_linha').value;
+    console.log('null || undefined =>',pessoa_ID)
+
+
+    fetch(`${url}/pessoa/${pessoa_ID}`, {
+        method: "DELETE",
+        //mode: 'no-cors'
+        
+    })
+    
+        .then(response => {
+            console.log(response.status);
+        })
+        
+        .catch(error => {
+            console.log('Erro ao tentar excluir a pessoa', error);
+        });
+    }
+    
+
+
+    
 
 
 
