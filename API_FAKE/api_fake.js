a569fd6c4ccd58853fa89ef560f41b91aaa1e008
m4th3usfp <141687871+m4th3usfp@users.noreply.github.com>
2024-03-08 02:42:09 -0300

    so falta o editar_linha

--- API_FAKE/api_fake.js
+++ API_FAKE/api_fake.js
@@ -3,13 +3,106 @@ const pessoasSalvas = [{}];
 const idsalvo = [{}];
 
 document.addEventListener('DOMContentLoaded', function () {
-    let url = document.querySelector('#URL')
 
+    let url = document.querySelector('#URL').value
+    console.log('zuado', url)
+
+    if (url.length > 0) {
+
+        fetch(url + '/pessoa', {
+            method: 'GET',
+
+        })
+
+            .then(response => {
+
+                return response.json()
+
+
+            })
+            .then(response => {
+
+
+
+                console.log('response do GET =>', response)
+
+                for (pessoa of response) {
+
+                    if (pessoa.tipo === '1') {
+                        pessoa.tipo = 'Cliente';
+                    } else if (pessoa.tipo === '2') {
+                        pessoa.tipo = 'Fornecedor';
+                    } else if (pessoa.tipo === '3') {
+                        pessoa.tipo = 'Empregado';
+                    } else {
+                        pessoa.tipo = 'Tipo desconhecido';
+                    }
+
+                    const tbody = document.querySelector('#tbody');
+                    let pessoa_ID = pessoa._id;
+                    let editar_ID = pessoa_ID;
+
+                    idsalvo.push(pessoa_ID)
+
+                    console.log('id do GET => ', pessoa_ID)
+
+                    if (tbody.querySelectorAll('tr').length === 0) {
+                        const headerRow = document.createElement('tr');
+                        headerRow.innerHTML = `
+                            <td>Nome</td>
+                            <td>Email</td>
+                            <td>Tipo</td>
+                            <td></td>
+                            </tr>
+                            `;
+                        tbody.appendChild(headerRow);
+
+
+                    }
+
+                    if (tbody.querySelectorAll('tr').length >= 1) {
+                        document.getElementById('h5').style.display = 'none';
+                    }
+
+
+                    const newRow = document.createElement('tr');
+                    newRow.innerHTML = `
+                        <td>${pessoa.nome}</td>
+                        <td>${pessoa.email}</td>
+                        <td>${pessoa.tipo}</td>
+                        <td>
+                            <button id="editar_pessoa" class="btn btn-secondary" type="button" data-bs-toggle="modal" value=${editar_ID} onclick="editarLinha(this)">Editar</button>
+                            <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" value=${pessoa_ID} onclick="cancelarLinha(this)">Cancelar</button>
+                        </td>
+                        
+                        `;
+
+                    tbody.appendChild(newRow);
+
+
+
+
+
+                }
+
+
+
+            })
+
+    }
 
     let urlUsada = '';
 
-    url.addEventListener('blur', function () {
-        const URL = url.value;
+    let inputurl = document.querySelector('#URL')
+
+
+
+    if (url.length > 0) {
+        return
+    }
+    inputurl.addEventListener('blur', function () {
+
+        const URL = inputurl.value;
         if (urlUsada === '') {
             urlUsada = URL
             console.log('e isso', URL)
@@ -27,8 +120,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     })
                     .then(response => {
-                       
-                        
+
+
 
                         console.log('response do GET =>', response)
 
@@ -50,7 +143,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                             idsalvo.push(pessoa_ID)
 
-                            console.log('id do GET => ',pessoa_ID)
+                            console.log('id do GET => ', pessoa_ID)
 
                             if (tbody.querySelectorAll('tr').length === 0) {
                                 const headerRow = document.createElement('tr');
@@ -84,9 +177,9 @@ document.addEventListener('DOMContentLoaded', function () {
                                 `;
 
                             tbody.appendChild(newRow);
-                            
-                            
-                            
+
+
+
 
 
                         }
@@ -125,9 +218,9 @@ function inserirDados(form) {
     const email = form.querySelector('input[name="email"]').value;
     let tipo = form.querySelector('input[name="tipo"]:checked').value;
     const URL = document.getElementById('URL').value;
-    
-    
-    
+
+
+
 
 
     if (event.submitter.id === 'cancelar_modal') {
@@ -153,14 +246,14 @@ function inserirDados(form) {
         body: JSON.stringify(novaPessoa)
     })
         .then((response) => {
-            console.log('response post =>',response)
+            console.log('response post =>', response)
             return response.json();
         })
         .then((response) => {
             let pessoa_ID = response._id;
             let editar_ID = pessoa_ID;
 
-            console.log('id da pessoa => ',pessoa_ID)
+            console.log('id da pessoa => ', pessoa_ID)
 
             pessoasSalvas.push(novaPessoa);
 
@@ -210,13 +303,14 @@ function inserirDados(form) {
               `;
 
             tbody.appendChild(newRow);
-            
-            
+
+
 
             if (tbody.querySelectorAll('tr').length >= 1) {
                 document.getElementById('h5').style.display = 'none';
                 alert('Pessoa adicionada');
                 tbody.style.display = '';
+
             } else {
 
                 document.getElementById('h5').style.display = 'block';
@@ -236,106 +330,121 @@ function cancelarLinha(botao) {
     let pessoa_ID = botao.value;
     const url = document.querySelector('#URL').value;
 
+
+    confirm('Deseja deletar essa pessoa ?')
+
     fetch(`${url}/pessoa/${pessoa_ID}`, {
         method: "DELETE",
     })
-    .then(response => {
-        console.log(response.status);
+        .then(response => {
+            console.log(response.status);
 
-        alert('Tem certeza que quer deletar essa pessoa ? ')
+            alert('Deletado com sucesso XD')
 
-        linha.remove();
+            linha.remove();
 
-        if (tbody.querySelectorAll('tr').length === 1) {
-            
+            if (tbody.querySelectorAll('tr').length === 1) {
 
-            tbody.style.display = 'none';
 
-            document.querySelector('h5').style.display = 'block';
-        }
-    })
-    .catch(error => {
-        console.log('Erro ao tentar excluir a pessoa', error);
-    });
+                tbody.style.display = 'none';
+
+                document.querySelector('h5').style.display = 'block';
+            }
+        })
+        .catch(error => {
+            console.log('Erro ao tentar excluir a pessoa', error);
+        });
 }
 
 function editarLinha(botao) {
-    console.log(botao)
-    let username = botao.querySelector('input[name="nome"]').value;
-    let useremail = botao.querySelector('input[name="email"]').value;
-    let tipo = botao.querySelector('input[name="tipo_editar"]:checked').value;
+    console.log('o botao aqqui =>', botao)
+    let username = document.querySelector('input[name="nome"]').value;
+    let useremail = document.querySelector('input[name="email"]').value;
+    let tipo = document.querySelector('input[name="tipo"]:checked').value;
     const url = document.querySelector('#URL').value;
     let editar_ID = botao.value;
-    let pessoa_ID = editar_ID 
 
-    const novaPessoa = { username, useremail, tipo, url, editar_ID, pessoa_ID };
 
-        
+    const novaPessoa = { username, useremail, tipo, url, editar_ID };
+
+    console.log('ta aqui', novaPessoa)
+
+    // let inputform = document.querySelector('#form_pessoa')
+    //         console.log(inputform)
 
     let dadosAtualizados = {
         nome: username,
         email: useremail,
         tipo: tipo,
         editarID: editar_ID,
-        pessoaID: pessoa_ID
+
     }
-    
+
+
     fetch(`${url}/pessoa/${editar_ID}`, {
         method: "PUT",
         headers: {
             "Content-Type": "Aplication/json",
             "Accept": "Aplication/json"
         },
-        body: json.stringify(dadosAtualizados)
-    })
-    .then(response => {
-        if(!response.ok) {
-            throw new Error('Erro ao editar linha')
-        }
-        return response.json();
-    })
-    .then(error => {
-        console.error('Error: ', error);
-    })
- 
-    .then(response => {
-        let editar_ID = response._id;
-        console.log('id do editar => ', editar_ID)
-        pessoasSalvas.push(novaPessoa);
-        idsalvo.push(editar_ID);
+        body: JSON.stringify(dadosAtualizados)
+        })
 
-        
-    if(tipo === '1') {
-        tipo = 'Cliente';
-    } else if(tipo === '2') {
-        tipo = 'Fornecedor';
-    }else if(tipo === '3') {
-        tipo = 'Empregado';
-    } else {
-        tipo = 'Tipo desconhecido';
-    }
-
-    const tbody = document.querySelectorAll('#tbody')[0];
-
-    const newRow = document.createElement('tr');
-            newRow.innerHTML = `
-              <td>${username}</td>
-              <td>${useremail}</td>
-              <td>${tipo}</td>
-              <td>
-                  <button id="editar_pessoa" class="btn btn-secondary" type="button" data-bs-toggle="modal" value=${editar_ID} onclick="editarLinha(this)">Editar</button>
-                  <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" value=${pessoa_ID} onclick="cancelarLinha(this)">Cancelar</button>
-              </td>
-              
-              `;
-
-            tbody.appendChild(newRow);
-
-    })
-   };
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao editar linha')
+            }
+            return response.json();
+        })
+        .then(response => {
+            
+        })
 
 
 
+    // .then(response => {
+    //     let editar_ID = response._id;
+
+
+
+    //     console.log('id do editar => ', editar_ID)
+
+
+    //     if (tipo === '1') {
+    //         tipo = 'Cliente';
+    //     } else if (tipo === '2') {
+    //         tipo = 'Fornecedor';
+    //     } else if (tipo === '3') {
+    //         tipo = 'Empregado';
+    //     } else {
+    //         tipo = 'Tipo desconhecido';
+    //     }
+
+
+    //     const tbody = document.querySelectorAll('#tbody')[0];
+
+
+
+    //     const newRow = document.createElement('tr');
+    //     newRow.innerHTML = `
+    //       <td>${username}</td>
+    //       <td>${useremail}</td>
+    //       <td>${tipo}</td>
+    //       <td>
+    //           <button id="editar_pessoa" class="btn btn-secondary" type="button" data-bs-toggle="modal" value=${editar_ID} onclick="editarLinha(this)">Editar</button>
+    //           <button id="cancelar_linha" class="btn btn-danger" type="button" data-bs-dismiss="modal" value=${pessoa_ID} onclick="cancelarLinha(this)">Cancelar</button>
+    //       </td>
+
+    //       `;
+
+    //     tbody.appendChild(newRow);
+
+    //     pessoasSalvas.push(novaPessoa);
+    //     idsalvo.push(editar_ID);
+
+
+    // })
+};
 
 
 
@@ -344,19 +453,22 @@ function editarLinha(botao) {
 
 
 
-        
-
-        
-    
-    
-
-    
-  
-    
-    
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
